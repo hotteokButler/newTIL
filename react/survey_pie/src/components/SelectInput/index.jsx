0a94fd3e137c05f72b9546b622bcad25f215dc37
hotteokButler <ygs12567@gmail.com>
2025-01-14 13:44:26 +0900

    Add 입력 setAnswer 추가

--- react/survey_pie/src/components/SelectInput/index.jsx
+++ react/survey_pie/src/components/SelectInput/index.jsx
@@ -4,7 +4,7 @@ export default function TextInput({ answer, setAnswer }) {
       type="text"
       name=""
       id=""
-      value={answer ? answer : ''}
+      value={answer ? answer : ''} // answer 없으면 공백 처리 필요 => 렌더링 이슈 발생
       onChange={(e) => {
         setAnswer(e.target.value);
       }}
