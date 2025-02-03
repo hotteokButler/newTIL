dc34b50750fa4f3e0e57269841eb20c9d8c78639
hotteokButler <ygs12567@gmail.com>
2025-02-03 21:54:11 +0900

    Add : dummy data 사용을 위해 json-server 라이브러리 이용
    -- JSON 파일을 이용해 모의용 RESTful API 서버 및 DB를 구축가능
    -- 테스트용

--- react/survey_pie/pages/CompletionPage/index.jsx
+++ react/survey_pie/pages/CompletionPage/index.jsx
@@ -1,10 +1,14 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 
-import testState from '../../src/stores/test/atom';
 import testWithComma from '../../src/stores/test/testWithComma';
 import * as S from './CompletionPage.styled';
 
 export default function CompletionPage() {
   const test = useRecoilValue(testWithComma);
+
+  useEffect(() => {}, []);
+
   return <S.CompletionPageWrapper>{test}</S.CompletionPageWrapper>;
 }
---
+++ react/survey_pie_server/README.md
@@ -0,0 +1,5 @@
+# 예제 DB server
+
+### json server
+
+참고 블로그 [https://www.heropy.dev/p/zZdlXx]
