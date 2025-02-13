19f8f1253f4de704ee066d78803fa6dc7a7518d2
hotteokButler <ygs12567@gmail.com>
2025-02-13 22:58:29 +0900

    Add : useSWR을 이용하여 data fetch - [SWR 장점]   1)Cache : 백그라운드에서 캐시를 재검증하는 동안 기존 캐시 데이터 보여줌   2)중복제거 :  전역캐시를 이용해 모든 컴포넌트 사이 저장 공유 가   3) 깊은 비교 : data 값이나 api 변경되지 않으면 리렌더링 하지 않음   4) 자동갱신 : 원하는 시점에 갱신 가능

--- react/survey-pie-admin/src/pages/ListPage.jsx
+++ react/survey-pie-admin/src/pages/ListPage.jsx
@@ -1,6 +1,19 @@
+import useSwr from 'swr';
+
 import MainLayout from '../layouts/MainLayout';
+import fetcher from '../lib/fetcher';
 
 const ListPage = () => {
+  /* 처음 요청 보낸 후 data가 떨어지는 기간동안에는 undefined 상태이며 data를 받게되면 다시 렌더링이 되며,
+   두번 렌더링이 됨 useSWR을 사용하면 이렇게 되더라도 주소나 값이 바뀌지 않는 이상 api는 최초 한번만 불러옴 */
+  const { data, error } = useSwr('/surveys', fetcher);
+
+  console.log(data);
+
+  if (error) {
+    return 'error';
+  }
+
   return <MainLayout selectedKeys={['list']}>ListPage</MainLayout>;
 };
 
