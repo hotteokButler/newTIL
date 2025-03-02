f72f3bc1c4fd0bc37d8da112ea0f608e364fad28
hotteokButler <ygs12567@gmail.com>
2025-03-02 22:44:33 +0900

    Add : Pagination - Ant design Table에 pagination 연결

--- react/survey-pie-admin/src/pages/ListPage.jsx
+++ react/survey-pie-admin/src/pages/ListPage.jsx
@@ -1,8 +1,12 @@
 import { Table } from 'antd';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import useSwr from 'swr';
 
 import MainLayout from '../layouts/MainLayout';
 import fetcher from '../lib/fetcher';
+
+const PAGE_SIZE = 20;
 
 const columns = [
   {
@@ -42,8 +46,8 @@ const ListPage = () => {
   /* 처음 요청 보낸 후 data가 떨어지는 기간동안에는 undefined 상태이며 data를 받게되면 다시 렌더링이 되며,
    두번 렌더링이 됨 useSWR을 사용하면 이렇게 되더라도 주소나 값이 바뀌지 않는 이상 api는 최초 한번만 불러옴 */
   const { data, error } = useSwr('/surveys', fetcher);
-
-  console.log(data);
+  const navigate = useNavigate();
+  const [page, setPage] = useState(1);
 
   if (error) {
     return 'error';
@@ -54,7 +58,23 @@ const ListPage = () => {
   return (
     <MainLayout selectedKeys={['list']}>
       <Table
+        onRow={({ id }) => {
+          return {
+            onClick: () => {
+              navigate(`/builder/${id}`);
+            },
+          };
+        }}
         columns={columns}
+        pagination={{
+          position: ['bottomCenter'],
+          total: data.length,
+          current: page,
+          pageSize: PAGE_SIZE,
+        }}
+        onChange={(pagination) => {
+          setPage(pagination.current);
+        }}
         dataSource={data.map((item) => ({ ...item, key: item.id }))}
       />
     </MainLayout>
