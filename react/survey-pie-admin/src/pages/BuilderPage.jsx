7b241bd75a892023a8492b64472b393d5313598c
hotteokButler <ygs12567@gmail.com>
2025-03-02 22:52:45 +0900

    Style : Ant design grid system
    - flex 항목으로 가변,고정 가능

--- react/survey-pie-admin/src/pages/BuilderPage.jsx
+++ react/survey-pie-admin/src/pages/BuilderPage.jsx
@@ -1,7 +1,16 @@
+import { Col, Row } from 'antd';
+
 import MainLayout from '../layouts/MainLayout';
 
 const BuilderPage = () => {
-  return <MainLayout selectedKeys={['builder']}>BuilderPage</MainLayout>;
+  return (
+    <MainLayout selectedKeys={['builder']}>
+      <Row gutter={[16, 16]}>
+        <Col flex="auto">col1</Col>
+        <Col flex="300px">col2</Col>
+      </Row>
+    </MainLayout>
+  );
 };
 
 export default BuilderPage;
