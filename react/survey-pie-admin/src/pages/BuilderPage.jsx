4d0486d862ff41153bff09bb8a8eacbde07a73c7
hotteokButler <ygs12567@gmail.com>
2025-03-14 17:37:00 +0900

    Add : middleware 연결 - 초기값 null 셋팅을 위해 data 키 안에 들어가도록 변경

--- react/survey-pie-admin/src/pages/BuilderPage.jsx
+++ react/survey-pie-admin/src/pages/BuilderPage.jsx
@@ -1,21 +1,39 @@
 import { Col, Input, Row } from 'antd';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 import OptionSection from '../components/OptionSection';
 import PreviewSection from '../components/PreviewSection';
 import MainLayout from '../layouts/MainLayout';
+import fetcher from '../lib/fetcher';
 import {
   addQuestion,
   deleteQuestion,
   moveDownQuestion,
   moveUpQuestion,
+  setSurvey,
   setTitle,
 } from '../stores/survey/surveySlice';
 
 const BuilderPage = () => {
-  const surveyData = useSelector((state) => state.survey); // store data 구독
+  const surveyData = useSelector((state) => state.survey.data); // store data 구독
   const dispatch = useDispatch();
+  const { surveyId } = useParams();
 
+  useEffect(() => {
+    //rendering 될 때마다 실행되지 않게 useEffect안에서 호출
+    dispatch((dispatch, getState) => {
+      //최종적으로 실행될 때 store내 middleware 최종 action 인자를 받음
+      // api 호출 -> fetcher 만들어 놓은 것을 통해 호출
+      fetcher(`/surveys/${surveyId}`).then((res) => {
+        //data를 다시 dispatch를 통해 store에 저장
+        dispatch(setSurvey(res));
+      });
+    });
+  }, [dispatch, surveyId]);
+
+  if (!surveyData) return null;
   return (
     <MainLayout selectedKeys={['builder']}>
       <Row gutter={[16, 16]}>
