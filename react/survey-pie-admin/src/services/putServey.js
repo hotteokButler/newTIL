7a30a052a71997e05007594bc6993eed7c1cdb84
hotteokButler <ygs12567@gmail.com>
2025-03-24 18:48:04 +0900

    Add: 저장 완료 실패 alert 설정

--- react/survey-pie-admin/src/services/putServey.js
+++ react/survey-pie-admin/src/services/putServey.js
@@ -1,6 +1,15 @@
 import axios from 'axios';
 
 const putSurvey = (survey) => {
-  axios.put(`/surveys/${survey.id}`, survey).then('저장되었습니다.');
+  axios
+    .put(`/surveys/${survey.id}`, survey)
+    .then(
+      (res) =>
+        res.status === 200 && alert('정상적으로 저장이 완료되었습니다😎'),
+    )
+    .catch((err) => {
+      console.log(err);
+      alert('예기치 못한 오류가 발생했습니다😭');
+    });
 };
 export default putSurvey;
