90fbbb226d97bf858d2e2695545e5c1de581581e
hotteokButler <ygs12567@gmail.com>
2025-03-27 00:49:05 +0900

    Fix : 질문 list 최신순서 상위 정렬

--- react/survey-pie-admin/src/pages/ListPage.jsx
+++ react/survey-pie-admin/src/pages/ListPage.jsx
@@ -54,7 +54,7 @@ const columns = [
 const ListPage = () => {
   /* 처음 요청 보낸 후 data가 떨어지는 기간동안에는 undefined 상태이며 data를 받게되면 다시 렌더링이 되며,
    두번 렌더링이 됨 useSWR을 사용하면 이렇게 되더라도 주소나 값이 바뀌지 않는 이상 api는 최초 한번만 불러옴 */
-  const { data, error } = useSwr('/surveys', fetcher);
+  const { data, error } = useSwr('/surveys?_sort=id&_order=desc', fetcher);
   const navigate = useNavigate();
   const [page, setPage] = useState(1);
 
