ff3fdeec1f6402fbf79bd9f8d1af5d8c902d365e
hotteokButler <ygs12567@gmail.com>
2025-03-27 21:19:01 +0900

    Fix : 삭제 후 useSWR 확률적 바로 반영 안되는 오류 수정
    - 리스트를 불러오는 키와 바인딩 된 mutate 함수를 활용하여 삭제 후에 갱신
    - bounded mutate : useSWR  함수에 전달된 키와 연결된 키는 캐시에서 데이터를 찾을 때 사용되며, 이렇게 찾은 데이터는 첫 번째 인자로 반환

--- react/survey-pie-admin/src/pages/ListPage.jsx
+++ react/survey-pie-admin/src/pages/ListPage.jsx
@@ -1,6 +1,6 @@
 import { DeleteFilled, PlusCircleFilled } from '@ant-design/icons';
 import { Table } from 'antd';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useSwr from 'swr';
 
@@ -11,52 +11,61 @@ import * as S from './pages.styled';
 
 const PAGE_SIZE = 20;
 
-const columns = [
-  {
-    title: '번호',
-    dataIndex: 'id',
-    key: 'id',
-  },
-  {
-    title: '제목',
-    dataIndex: 'title',
-    key: 'title',
-  },
-  {
-    title: '생성일',
-    dataIndex: 'createdAt',
-    key: 'createdAt',
-    render: (_, { createdAt }) => {
-      const time = new Date(createdAt);
-      return `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
-    },
-  },
-  {
-    title: '액션',
-    dataIndex: 'id',
-    key: 'action',
-    render: (_, record) => {
-      return (
-        <S.ListDeleteButton
-          type="button"
-          onClick={(e) => {
-            e.stopPropagation();
-            deleteSurvey(record);
-          }}
-        >
-          <DeleteFilled />
-        </S.ListDeleteButton>
-      );
-    },
-  },
-];
-
 const ListPage = () => {
   /* 처음 요청 보낸 후 data가 떨어지는 기간동안에는 undefined 상태이며 data를 받게되면 다시 렌더링이 되며,
    두번 렌더링이 됨 useSWR을 사용하면 이렇게 되더라도 주소나 값이 바뀌지 않는 이상 api는 최초 한번만 불러옴 */
-  const { data, error } = useSwr('/surveys?_sort=id&_order=desc', fetcher);
+  const { data, error, mutate } = useSwr(
+    '/surveys?_sort=id&_order=desc',
+    fetcher,
+  );
   const navigate = useNavigate();
   const [page, setPage] = useState(1);
+
+  const columns = useMemo(
+    () => [
+      {
+        title: '번호',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: '제목',
+        dataIndex: 'title',
+        key: 'title',
+      },
+      {
+        title: '생성일',
+        dataIndex: 'createdAt',
+        key: 'createdAt',
+        render: (_, { createdAt }) => {
+          const time = new Date(createdAt);
+          return `${time.getFullYear()}-${
+            time.getMonth() + 1
+          }-${time.getDate()}`;
+        },
+      },
+      {
+        title: '액션',
+        dataIndex: 'id',
+        key: 'action',
+        render: (_, record) => {
+          return (
+            <S.ListDeleteButton
+              type="button"
+              onClick={(e) => {
+                e.stopPropagation();
+                deleteSurvey(record);
+                mutate();
+              }}
+            >
+              <DeleteFilled />
+            </S.ListDeleteButton>
+          );
+        },
+      },
+    ],
+    [mutate],
+  );
 
   if (error) {
     return 'error';
