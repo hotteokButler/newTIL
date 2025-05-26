843fb82556e047c035ec11b5dff006057a0b1462
hotteokButler <ygs12567@gmail.com>
2025-05-26 18:59:24 +0900

    [Next js] page router - routnig

---
+++ next_js/01_Page_Router/page_router/src/pages/search/index.tsx
@@ -0,0 +1,11 @@
+import { useRouter } from 'next/router';
+
+export default function Page() {
+	const router = useRouter();
+
+	const { q } = router.query;
+
+	console.log(q);
+
+	return <div>Search Page</div>;
+}
