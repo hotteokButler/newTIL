d6dc35b45d07f80fabee05add25b32ddc9389409
hotteokButler <ygs12567@gmail.com>
2025-05-30 18:06:24 +0900

    [Next - page router ] -  arrow function (함수 표현식) 사용 시 코드가 더 간결하고 편리하다

--- next_js/01_Page_Router/page_router/src/pages/index.tsx
+++ next_js/01_Page_Router/page_router/src/pages/index.tsx
@@ -1,15 +1,12 @@
 import SearchableLayout from '@/components/layout/searchable-layout';
 import type { NextPageWithLayout } from './_app';
-import { JSX } from 'react';
 
-function Home(): JSX.Element {
+const Home: NextPageWithLayout = () => {
 	return <p>hello world</p>;
-}
+};
 
-const Page = Home as NextPageWithLayout;
-
-Page.getLayout = (page: React.ReactNode) => {
+Home.getLayout = function getLayout(page: React.ReactNode) {
 	return <SearchableLayout>{page}</SearchableLayout>;
 };
 
-export default Page;
+export default Home;
