79308fb7db9db7c10d2e3c7b609815bd3d8063ea
hotteokButler <ygs12567@gmail.com>
2025-05-31 13:35:14 +0900

    mock data 추가

--- next_js/01_Page_Router/page_router/src/pages/index.tsx
+++ next_js/01_Page_Router/page_router/src/pages/index.tsx
@@ -1,8 +1,22 @@
 import SearchableLayout from '@/components/layout/searchable-layout';
 import type { NextPageWithLayout } from './_app';
 
+import style from '../styles/index.module.css';
+
 const Home: NextPageWithLayout = () => {
-	return <p>hello world</p>;
+	return (
+		<div className={style.container_wrap}>
+			{/* 지금 추천하는 도서 ====== */}
+			<section>
+				<h3>지금 추천하는 도서</h3>
+			</section>
+
+			{/* 등록된 모든 도서 ====== */}
+			<section>
+				<h3>등록된 모든 도서</h3>
+			</section>
+		</div>
+	);
 };
 
 Home.getLayout = function getLayout(page: React.ReactNode) {
