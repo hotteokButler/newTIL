1a4ddafa0162875c2d7999fa7d024e1869e4d2b4
hotteokButler <ygs12567@gmail.com>
2025-05-31 14:36:40 +0900

    Style: main book list ui

--- next_js/01_Page_Router/page_router/src/components/book-item.tsx
+++ next_js/01_Page_Router/page_router/src/components/book-item.tsx
@@ -1,5 +1,19 @@
+import { IBookData } from '@/type/types';
+import Link from 'next/link';
 import React from 'react';
+import style from '@/styles/book-item.module.css';
 
-export default function BookItem() {
-	return <div>BookItem</div>;
+export default function BookItem({ id, title, subTitle, description, author, publisher, coverImgUrl }: IBookData) {
+	return (
+		<Link href={`/book/${id}`} className={style.book_item_card}>
+			<img src={coverImgUrl} alt={title} />
+			<dl>
+				<dt>{title}</dt>
+				<dd>{subTitle}</dd>
+				<dd>
+					{author} | {publisher}
+				</dd>
+			</dl>
+		</Link>
+	);
 }
--- next_js/01_Page_Router/page_router/src/pages/index.tsx
+++ next_js/01_Page_Router/page_router/src/pages/index.tsx
@@ -3,17 +3,26 @@ import type { NextPageWithLayout } from './_app';
 
 import style from '../styles/index.module.css';
 
+import books from '@/mock/mock.json';
+import BookItem from '@/components/book-item';
+
 const Home: NextPageWithLayout = () => {
 	return (
 		<div className={style.container_wrap}>
 			{/* 지금 추천하는 도서 ====== */}
 			<section>
 				<h3>지금 추천하는 도서</h3>
+				{books.map((book) => (
+					<BookItem key={book.id} {...book} />
+				))}
 			</section>
 
 			{/* 등록된 모든 도서 ====== */}
 			<section>
 				<h3>등록된 모든 도서</h3>
+				{books.map((book) => (
+					<BookItem key={book.id} {...book} />
+				))}
 			</section>
 		</div>
 	);
