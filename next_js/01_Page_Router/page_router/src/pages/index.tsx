1e7e6c5219fecd7c6c7f93cce47530874a420a9e
hotteokButler <ygs12567@gmail.com>
2025-06-02 13:15:42 +0900

    [Next] - InferGetServerSidePropsType - https://nextjs.org/docs/pages/api-reference/functions/get-server-side-props
    - getServerSideProps 함수의 반환값 타입을 자동으로 추론해주는 기능을 하는 타입
        -> 제네릭으로 typeof getServerSideProps함수 넣어두면 자동으로 함수의 반환값 타입이 추론되어 매개변수 타입으로 정의됨

--- next_js/01_Page_Router/page_router/src/pages/index.tsx
+++ next_js/01_Page_Router/page_router/src/pages/index.tsx
@@ -5,10 +5,20 @@ import style from '../styles/index.module.css';
 
 import books from '@/mock/mock.json';
 import BookItem from '@/components/book-item';
+import { InferGetServerSidePropsType } from 'next';
 
-export const getServerSideProps = () => {};
+export const getServerSideProps = () => {
+	const data = 'hello';
+	return {
+		props: {
+			data,
+		},
+	};
+};
 
-const Home: NextPageWithLayout = () => {
+const Home: NextPageWithLayout = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+	console.log(data);
+
 	return (
 		<div className={style.container_wrap}>
 			{/* 지금 추천하는 도서 ====== */}
