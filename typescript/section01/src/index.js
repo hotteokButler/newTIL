35295ebfd9e525203898b0ef69f01fa02f3d30ed
hotteokButler <ygs12567@gmail.com>
2025-06-25 00:02:09 +0900

    [Ts] Fix : Cannot find module 'undici-types'. 오류
    - skipLibCheck 옵션 true : TypeScript는 프로젝트 내에서 참조하는 라이브러리 파일(.d.ts)의 타입 정보를 검사하지 않는다
    - 타입 검사를 생략하므로, 라이브러리 코드의 변경으로 인해 발생할 수 있는 타입 오류가 생길 수 있으므로 실제  프로젝트에서는 설정하지 않는게 좋을 것 같다고 생각함

--- typescript/section01/src/index.js
+++ typescript/section01/src/index.js
@@ -1,4 +1,2 @@
-// ES6 버전에서 추가된 arrow function의 경우 함수 표현식으로 바뀌어 변환됨
-var func = function () {
-	return console.log('arrow function');
-};
+// target : "ESNext" 옵션 설정 시 최신버전
+const func = () => console.log('arrow function');
--- typescript/section01/src/index.ts
+++ typescript/section01/src/index.ts
@@ -1,3 +1,2 @@
-// ES6 버전에서 추가된 arrow function의 경우 함수 표현식으로 바뀌어 변환됨
-
+// target : "ESNext" 옵션 설정 시 최신버전
 const func = () => console.log('arrow function');
