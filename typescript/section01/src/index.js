e5b3a3f85b3021fe7360a353e35086a381ec376e
hotteokButler <ygs12567@gmail.com>
2025-06-25 16:43:48 +0900

    [TS] - outDir : 컴파일하는 결과가 위치할 경로 설정 -  컴파일하는 결과로 생성되는 코드를 작성하는 코드 영역에서 분리 가능

--- typescript/section01/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const hello_1 = require("./hello");
-// target : "ESNext" 옵션 설정 시 최신버전
-const func = () => console.log('arrow function');
-(0, hello_1.hello)();
