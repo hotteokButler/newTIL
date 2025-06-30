6afb80039faf7db444553c218a994038e6e4d9ba
hotteokButler <ygs12567@gmail.com>
2025-06-30 19:24:07 +0900

    [next] - dynamic 옵션 - 페이지의 유형을 강제로 Statc 또는 Dynamic으로 설정하는 옵션 - auto / force-dynamic(강제 동적) / force-static(강제 정적) / error (강제 정적, 못할 이유 있을 시 빌드 시 오류)

--- next_js/02_App_router/app_router/src/app/(with-searchbar)/page.tsx
+++ next_js/02_App_router/app_router/src/app/(with-searchbar)/page.tsx
@@ -3,6 +3,18 @@ import RandomBooks from '@/components/random-books';
 
 import style from './page.module.css';
 
+/* 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
+- 1. auto : 기본값, 아무것도 강제하지 않음
+- 2. force-dynamic : 강제로 Dynamic 페이지로 설정
+- 3. force-static : 강제로 Static으로 설정
+    -> 동적 함수 : 페이지 내부에서 사용되는 동적 함수 모두 undefined, 빈값을 반환하도록 설정됨
+		-> data fetching : no store 설정이 되어있더라고해도 강제적으로 캐싱이 되도록 변경됨
+		=> 부작용 : 정상적으로 둉작하지 않을 수 있음
+- 4. error : 페이지를 강제로 static 페이지로 강제로 설정 하나,
+            설정하면 안되는 이유(동적함수 설정 / 캐싱되지 않은 데이터 패칭)가 있다면 빌드시 오류 발생 
+*/
+export const dynamic = 'auto';
+
 export default function Home() {
 	return (
 		<div className={style.container}>
