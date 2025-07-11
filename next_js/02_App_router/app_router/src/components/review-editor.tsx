6133d8946ca575e4c515b6f9642b35dee1a1ff23
hotteokButler <ygs12567@gmail.com>
2025-07-11 18:00:06 +0900

    [Next] - client,server 두 군데 빈입력 방지 이유 - 클라이언트 측, 서버 측 각각을 모두 100% 신뢰할 수 없음- 안정성 및 보안 관점

--- next_js/02_App_router/app_router/src/components/review-editor.tsx
+++ next_js/02_App_router/app_router/src/components/review-editor.tsx
@@ -6,13 +6,17 @@ export default function ReviewEditor() {
 
 		const content = formData.get('content')?.toString();
 		const author = formData.get('author')?.toString();
+
+		if (!content || !author) {
+			return; // server action 종료
+		}
 	};
 
 	return (
 		<section>
 			<form action={submitReview}>
-				<input type='text' name='content' placeholder='리뷰내용' />
-				<input type='text' name='author' placeholder='작성자' />
+				<input required type='text' name='content' placeholder='리뷰내용' />
+				<input required type='text' name='author' placeholder='작성자' />
 				<button type='submit'>작성하기</button>
 			</form>
 		</section>
