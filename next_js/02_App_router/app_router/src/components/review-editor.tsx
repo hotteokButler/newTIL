6f47dd5fc1a293e1e2b45b614bd9074d6195e0e2
hotteokButler <ygs12567@gmail.com>
2025-07-11 17:29:42 +0900

    [Next] - server action - 스트링 타입으로 데이터 전달 시 기본 추론되는 타입을 string 타입으로 추론시켜야 하는 이유

--- next_js/02_App_router/app_router/src/components/review-editor.tsx
+++ next_js/02_App_router/app_router/src/components/review-editor.tsx
@@ -3,9 +3,12 @@ import React from 'react';
 export default function ReviewEditor() {
 	const submitReview = async (formData: FormData) => {
 		'use server';
-
-		console.log(formData.get('content'));
-		console.log(formData.get('author'));
+		const content = formData.get('content')?.toString();
+		/* FormDataEntryValue 는 string 또는 파일 타입을 의미하는 것이기 때문에,
+		 스트링 타입의 값을 전달받고 있는 상황에는 적절하지 않아 
+     toString 메서드를 통해 문자열 타입으로 변환하도록 설정 
+     */
+		const author = formData.get('author')?.toString();
 	};
 
 	return (
