c4661cd9a51ac90d308fdb65eecf7983ef7dcca0
hotteokButler <ygs12567@gmail.com>
2025-07-14 11:09:42 +0900

    [Next] - value만 바로 전달하는 hidden input에는 readOnly 속성을 추가해야함 - 읽기전용 속성 미설정 시 onChagne,defaultValue 없이 사용 오류메세지 뜸

--- next_js/02_App_router/app_router/src/components/review-editor.tsx
+++ next_js/02_App_router/app_router/src/components/review-editor.tsx
@@ -8,6 +8,7 @@ export default function ReviewEditor({ bookId }: { bookId: string }) {
 	return (
 		<section>
 			<form action={submitReview}>
+				{/* input에서 onChange event 없이 value로 바로 값을 전달하면 error이 발생하므로 readOnly 속성 추가 */}
 				<input name='bookId' value={bookId} hidden readOnly />
 				<input required type='text' name='content' placeholder='리뷰내용' />
 				<input required type='text' name='author' placeholder='작성자' />
