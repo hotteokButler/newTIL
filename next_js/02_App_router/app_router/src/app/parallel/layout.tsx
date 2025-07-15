0daeffc6f70c8421a3811401e6355120b3408f16
hotteokButler <ygs12567@gmail.com>
2025-07-15 14:06:40 +0900

    [Next] - parallel route

--- next_js/02_App_router/app_router/src/app/parallel/layout.tsx
+++ next_js/02_App_router/app_router/src/app/parallel/layout.tsx
@@ -1,8 +1,17 @@
+import Link from 'next/link';
 import { ReactNode } from 'react';
 
 export default function Layout({ children, sidebar, feed }: { children: ReactNode; sidebar: ReactNode; feed: ReactNode }) {
 	return (
 		<div>
+			<div style={{ color: 'tomato' }}>
+				<Link href={'/parallel'}>parallel</Link>
+				<br />
+				<Link href={'/parallel/setting'}>parallel setting</Link>
+			</div>
+			<br />
+			<br />
+
 			{sidebar}
 			{feed}
 			{children}
