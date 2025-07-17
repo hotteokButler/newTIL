0efd20c9fb661e8f7ad2ea17e1bcbf928a13b979
hotteokButler <ygs12567@gmail.com>
2025-07-17 10:52:20 +0900

    [Next] - 페러렐 라우터 + 인터셉트 라우터 활용한 모달

--- next_js/02_App_router/app_router/src/app/@modal/(.)book/[id]/page.tsx
+++ next_js/02_App_router/app_router/src/app/@modal/(.)book/[id]/page.tsx
@@ -4,7 +4,6 @@ import Modal from '@/components/modal/modal';
 export default function Page(props: any) {
 	return (
 		<div>
-			가로채기 성공
 			<Modal>
 				<BookDetailPage {...props} />
 			</Modal>
--- next_js/02_App_router/app_router/src/app/layout.tsx
+++ next_js/02_App_router/app_router/src/app/layout.tsx
@@ -6,8 +6,10 @@ import style from './layout.module.css';
 
 export default function RootLayout({
 	children,
+	modal,
 }: Readonly<{
 	children: React.ReactNode;
+	modal: React.ReactNode;
 }>) {
 	return (
 		<html lang='ko'>
@@ -19,7 +21,7 @@ export default function RootLayout({
 					<main>{children}</main>
 					<footer>제작 @winterlood</footer>
 				</div>
-
+				{modal}
 				{/* 모달을 띄워 줄 위치 설정 */}
 				<div id='modal-root'></div>
 			</body>
