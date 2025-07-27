c824bc48a4cbe1653e6fbafd1c06fb5d7995fe79
hotteokButler <ygs12567@gmail.com>
2025-07-27 11:58:16 +0900

    Style : 로그인, 회원가입 ui

--- next_js/z_com/src/app/(beforeLogin)/_components/SignupModal/index.tsx
+++ next_js/z_com/src/app/(beforeLogin)/_components/SignupModal/index.tsx
@@ -5,16 +5,17 @@ import { useRouter } from 'next/navigation';
 import { useRef, useState } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 
-import style from './loginModal.module.css';
+import style from './signupModal.module.css';
 
-const LoginModal = () => {
+const SignupModal = () => {
 	const router = useRouter();
-	const modalRef = useRef<HTMLDivElement>(null);
-	const [id, setId] = useState();
-	const [password, setPassword] = useState();
-	const [message, setMessage] = useState();
 
-	const onSubmit = () => {};
+	const modalRef = useRef<HTMLDivElement>(null);
+	const [id, setId] = useState('');
+	const [password, setPassword] = useState('');
+	const [nickname, setNickname] = useState('');
+	const [image, setImage] = useState<File>();
+	const [message, setMessage] = useState('');
 
 	const onClickClose = () => {
 		router.back();
@@ -24,6 +25,12 @@ const LoginModal = () => {
 
 	const onChangePassword = () => {};
 
+	const onChangeNickname = () => {};
+
+	const onChangeImageFile = () => {};
+
+	const onSubmit = () => {};
+
 	return (
 		<div className={style.modal_wrap}>
 			<div className={style.modal_con} ref={modalRef}>
@@ -31,7 +38,7 @@ const LoginModal = () => {
 					<AiOutlineClose />
 				</button>
 				<div className={style.modal_header}>
-					<h3>로그인하세요.</h3>
+					<h3>계정을 생성하세요.</h3>
 				</div>
 				<form onSubmit={onSubmit}>
 					<ul className={style.modal_body}>
@@ -43,16 +50,28 @@ const LoginModal = () => {
 								</label>
 							</div>
 							<div className={style.modal_input_con}>
+								<input type='text' name='name' id='name' className={style.modal_input} onChange={onChangeNickname} />
+								<label htmlFor='name' className={style.modal_label}>
+									닉네임
+								</label>
+							</div>
+							<div className={style.modal_input_con}>
 								<input type='password' id='password' name='password' className={style.modal_input} onChange={onChangePassword} />
 								<label htmlFor='password' className={style.modal_label}>
 									비밀번호
+								</label>
+							</div>
+							<div className={style.modal_input_con}>
+								<input type='file' name='image' id='image' className={style.modal_input} accept='image/*' onChange={onChangeImageFile} />
+								<label htmlFor='image' className={style.modal_label}>
+									프로필
 								</label>
 							</div>
 							<div className={style.modal_message}>{message}</div>
 						</li>
 						<li className={style.modal_footer}>
 							<button className={style.modal_action_btn} disabled={!id && !password}>
-								로그인하기
+								가입하기
 							</button>
 						</li>
 					</ul>
@@ -62,4 +81,4 @@ const LoginModal = () => {
 	);
 };
 
-export default LoginModal;
+export default SignupModal;
